b1e449fe9b0526382124b64aae73ed954e9a4e2b
Sewool <swcl309@naver.com>
2023-02-24 17:41:24 +0900

    20230224

--- extra_04/swea_10966.py
+++ extra_04/swea_10966.py
@@ -1,8 +1,8 @@
 ### 물놀이를 가자
 
 
-# import sys
-# sys.stdin= open("input.txt")
+import sys
+sys.stdin= open("sample_input.txt")
 
 from collections import deque
 
@@ -13,28 +13,24 @@ for T in range(1,TC+1):
     n,m = map(int,input().split())
     mat = []
     for i in range(n):
-        mat.append(input().strip())
+        mat.append(list(input().strip()))
     water = deque([])
-    land =  [[0]* m for i in range(n)]
     for i in range(n):
         for j in range(m):
             if mat[i][j] == "W":
                 water.append([i,j])
-                land[i][j] = 1
-            else:
-                land[i][j] = 0
-    ww = len(water)
+                mat[i][j] = 0
     res = 0
     while water:
         w = water.popleft()
         for i in range(4):
             nx = w[0] + dx[i]
             ny = w[1] + dy[i]
-            if 0<= nx < n and 0<= ny <m and not land[nx][ny]:
-                land[nx][ny] = land[w[0]][w[1]] + 1
-                res += land[w[0]][w[1]] + 1
+            if 0<= nx < n and 0<= ny <m and mat[nx][ny]== "L":
+                mat[nx][ny] = mat[w[0]][w[1]] + 1
+                res += mat[w[0]][w[1]] + 1
                 water.append([nx,ny])
-    print(f"#{T}",res +ww - m*n)
+    print(f"#{T}",res)
 
 
 
--- extra_04/test.py
+++ extra_04/test.py
@@ -1,4 +1,29 @@
-stack = [[x,y]]
-while :
-    x,y = stack.pop()
-    for i in range(4):
+import sys
+sys.stdin = open("sample_input.txt")
+
+
+
+
+TC = int(input())
+for T in range(1,TC+1):
+    n,m = map(int,input().split())
+    mat = []
+    for i in range(n):
+        mat.append(list(input().strip()))
+    water = []
+    for i in range(n):
+        for j in range(m):
+            if mat[i][j] == "W":
+                water.append([i,j])
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if mat[i][j] == "L":
+                cnt = m+n
+                for k in water:
+                    x,y = k
+                    dis = abs(i-x) + abs(j-y)
+                    if dis < cnt:
+                        cnt = dis
+                result += cnt
+    print(f"#{T}",result)
