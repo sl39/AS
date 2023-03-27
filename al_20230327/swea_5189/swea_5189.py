553eb0dc356da9dae3279200077e643506fc9aa2
swcl309 <swcl309@naver.com>
2023-03-27 21:42:25 +0900

    20230327

--- al_20230327/swea_5189/swea_5189.py
+++ al_20230327/swea_5189/swea_5189.py
@@ -1,20 +1,19 @@
 TC =int(input())
     
 
-def find(depth,cnt):
+def find(depth,cnt,next):
     global res
     
     if n == depth:
-        print(arr)
-        res = min(res,cnt)
+        res = min(res,cnt+mat[next][0])
         return
     
-    for i in range(n):
-        if i != depth:
-            if arr[i] == n:
-                arr[i] = depth
-                find(depth+1, cnt+mat[depth][i])
-                arr[i] = n
+    for i in range(1,n):
+        if mat[next][i] != 0:
+            if arr[i] == 0:
+                arr[i] = 1
+                find(depth+1, cnt+mat[next][i],i)
+                arr[i] = 0
 
 for T in range(1,TC+1):
     n = int(input())
@@ -22,15 +21,11 @@ for T in range(1,TC+1):
     for i in range(n):
         mat.append(list(map(int,input().split())))
     res = 5000
-    arr = [n]* n
-    find(0,0)
+    arr = [0]* n
+    for j in range(n):
+        arr[j] = 1
+        find(1,mat[0][j],j)
+        arr[j] = 0
+    print(f'#{T}',end=" ")
     print(res)
     
-    
-def check(arr):
-    visited = [0] * n
-    p = 0
-    t = 0 
-    while t <n:
-        if not visited[p]:
-            visited[p] = 1
