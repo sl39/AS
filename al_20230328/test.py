835626c9c60537942b1fc0301a8769cde180de3d
swcl309 <swcl309@naver.com>
2023-03-30 00:41:55 +0900

    20230330

--- al_20230328/test.py
+++ al_20230328/test.py
@@ -1,31 +1,27 @@
+def dock(idx, s,e, cnt):
+    global result
+
+    if idx == N:
+        result = max(result, cnt)
+        return
+
+    start, end = work[idx]
+    if cnt == 0:
+        dock(idx+1, start,end, cnt+1)
+    elif not(s<= start < e):
+        dock(idx+1, s,end, cnt+1)
+    dock(idx+1, s,e, cnt)
+
+
+    
+
 T = int(input())
-for _ in range(T):
-    arr = input()
-    arr = sorted(arr)
-    li = []
-    result = ''
-    visited = [0 for _ in range(6)]
 
-    for i in range(4):
-        if arr[i] == arr[i + 1] == arr[i + 2]:
-            li.append(arr[i])
-            li.append(arr[i+1])
-            li.append(arr[i+2])
-            visited[i] += 1
-            visited[i+1] += 1
-            visited[i+2] += 1
-    for i in range(4):
-        if visited[i] == 0:
-            if int(arr[i + 2]) - int(arr[i+1]) == 1 and int(arr[i+1]) - int(arr[i]) == 1:
-                li.append(arr[i])
-                li.append(arr[i+1])
-                li.append(arr[i+2])
-                visited[i] += 1
-                visited[i+1] += 1
-                visited[i+2] += 1
+for tc in range(1, T + 1):
+    N = int(input())
+    work = [list(map(int, input().split())) for _ in range(N)]
+    work.sort()
 
-
-    if len(li) == 6:
-        print(True)
-    else:
-        print(False)
+    result = 0
+    dock(0, 0,0, 0)
+    print(f'#{tc}',result)
